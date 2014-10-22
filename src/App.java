a34e6de8b87274a8fb4ee53159a1fbee67bf8d97
Roope Rantanen <roopepe@hotmail.com>
2014-10-23 00:58:04 +0300

    Created a new class App, which is the main class of the application.
    Signed-off-by: Roope Rantanen <roopepe@hotmail.com>

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+
+    public static void main(String[] args) {
+
+        GUI window = new GUI();
+
+        window.setTitle("CRYPTER");
+
+        window.setVisible(true);
+    }
+}
