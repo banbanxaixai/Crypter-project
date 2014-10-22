a6626cd2400623dccef5696b317ce1d58539bf30
Raido Roomets <raido.roomets@example.com>
2014-10-22 22:34:17 +0300

    Crypter.java: Added some documentation and modified array variables.
    Added information of authors, version and since for documentation.
    Changed both arrays to private static type for the whole class
    to use and documented their usage.

--- src/Crypter.java
+++ src/Crypter.java
@@ -1,17 +1,33 @@
+/**
+ * @author Raido Roomets <raido.roomets@cs.tamk.fi>
+ * @author Roope Rantanen <roope.rantanen@cs.tamk.fi>
+ * @version 2014-1022
+ * @since 1.7
+ */
 public class Crypter {
  
-    public static void main(String[] args) {
- 
-        String[] aakkoset = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+	/**
+	 * Array of alphabets and numbers.
+	 */
+    private static char[] alphabets = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
                 "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
                 "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8",
                 "9", "0", " " };
-        String[] morse = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.",
+
+	/**
+	 * Array of crypted figures.
+	 *
+	 * Each figure represents a character and is on the same index as
+	 * its counterpart in alphabets array.
+	 */
+	private static String[] morse = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.",
                 "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.",
                 "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-",
                 "-.--", "--..", ".----", "..---", "...--", "....-", ".....",
                 "-....", "--...", "---..", "----.", "-----", " " };
- 
+
+	public static void main(String[] args) {
+
         for (int i = 0; i < args.length; i++) {
  
             System.out.print(toMorse(aakkoset, morse, args[i]));
@@ -31,5 +47,4 @@ public class Crypter {
  
         return alphabeth;
     }
- 
 }
