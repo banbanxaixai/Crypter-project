9a652cb8c9a8f3b49234da96d997ddea82b23381
Roope Rantanen <roopepe@hotmail.com>
2014-10-23 00:45:39 +0300

    Created a new class GUI.
    Class creates and positions the components
    (buttons, textfields etc.) of the applications
    graphic user interface.
    
    Signed-off-by: Roope Rantanen <roopepe@hotmail.com>

---
+++ src/GUI.java
@@ -0,0 +1,57 @@
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+public class GUI extends JFrame {
+
+    private JButton startButton;
+    private JButton resetButton;
+    private JButton cryptButton;
+    private JTextArea originalText;
+    private JTextArea cryptedText;
+    private JPanel middleArea;
+
+    public GUI() {
+
+        super();
+
+        this.setSize(900, 300);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        BorderLayout bLayout = new BorderLayout();
+        FlowLayout fLayout = new FlowLayout();
+
+        setLayout(bLayout);
+
+        startButton = new JButton("START");
+
+        System.out.println(getHeight());
+
+        /** painike jota painamalla tekstikentät tyhjenevät */
+        resetButton = new JButton("RESET");
+
+        /** keskelle sijoitettava JPanel jonka sisälle wräpätään tekstialueet ja painike */
+        middleArea = new JPanel();
+
+        middleArea.setLayout(fLayout);
+
+        originalText = new JTextArea(15, 30);
+
+        cryptButton = new JButton("Crypt it!");
+
+        cryptedText = new JTextArea(15, 30);
+
+        /** Lisätään JPaneliin järjestyksessä 'elementit' */
+        middleArea.add(originalText);
+        middleArea.add(cryptButton);
+        middleArea.add(cryptedText);
+
+        add(resetButton, bLayout.PAGE_END);
+        add(middleArea, bLayout.CENTER);
+
+    }
+}
